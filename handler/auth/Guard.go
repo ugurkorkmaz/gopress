555784fa3b482adff08d5224efdf7c1cee582351
Uğur Korkmaz <korkmaz.uu@gmail.com>
2022-08-08 02:06:45 +0300

    fix: new skeleton and error handler add

--- handler/auth/Guard.go
+++ handler/auth/Guard.go
@@ -1,7 +1,14 @@
 package auth
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func Guard(c *fiber.Ctx) error {
-	return c.Next()
+	auth := true
+
+	if auth {
+		return c.Next()
+	}
+	return fiber.NewError(fiber.StatusUnauthorized, "not authorized")
 }
