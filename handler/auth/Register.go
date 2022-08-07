1839f50d631de13d9de4f30c4e841f7a966064cd
Uğur Korkmaz <korkmaz.uu@gmail.com>
2022-08-08 02:03:45 +0300

    doc: `swagger` @Success and @Failure return entity struct

--- handler/auth/Register.go
+++ handler/auth/Register.go
@@ -7,9 +7,8 @@ import "github.com/gofiber/fiber/v2"
 // @Tags auth
 // @Accept json
 // @Produce application/json
-// @Success 200 {array} ent.User
-// @Failure 400 {json} string
-// @Failure 500 {json} string
+// @Success 200 {array} entity.User
+// @Failure 400 {array} entity.Failure
 // @Router /api/auth/register [post]
 func Register(c *fiber.Ctx) error {
 	return c.SendString("Register")
