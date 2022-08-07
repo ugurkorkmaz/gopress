f7de5c9f0bf13fc8e5d0eb0674968e95d89e5788
Uğur Korkmaz <korkmaz.uu@gmail.com>
2022-08-07 06:19:54 +0300

    doc: swagger type doc add

--- handler/auth/Register.go
+++ handler/auth/Register.go
@@ -2,6 +2,15 @@ package auth
 
 import "github.com/gofiber/fiber/v2"
 
+// @Summary Register
+// @Description Register a new user
+// @Tags auth
+// @Accept json
+// @Produce application/json
+// @Success 200 {array} ent.User
+// @Failure 400 {json} string
+// @Failure 500 {json} string
+// @Router /api/auth/register [post]
 func Register(c *fiber.Ctx) error {
 	return c.SendString("Register")
 }
