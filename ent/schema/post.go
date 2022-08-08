d5e5b2a2f8bddc3b8054773b06f6b63d83607785
Uğur Korkmaz <korkmaz.uu@gmail.com>
2022-08-08 03:24:15 +0300

    feat: `post` from represents a reversed-edge `user` <- `post` vertices that has a back-reference to its source edge

--- ent/schema/post.go
+++ ent/schema/post.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
 	"github.com/google/uuid"
 )
@@ -29,5 +30,7 @@ func (Post) Fields() []ent.Field {
 
 // Edges of the Post.
 func (Post) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.From("author", User.Type).Ref("posts").Unique(),
+	}
 }
