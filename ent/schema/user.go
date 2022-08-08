f623235fc43dff09f531dddea5527961325725c4
Uğur Korkmaz <korkmaz.uu@gmail.com>
2022-08-08 03:00:49 +0300

    feat: `more_info` field json added to `User` schema

--- ent/schema/user.go
+++ ent/schema/user.go
@@ -21,7 +21,9 @@ func (User) Fields() []ent.Field {
 		field.String("email").NotEmpty().MinLen(3).MaxLen(128),
 		field.String("password").NotEmpty().MinLen(8),
 		field.Enum("role").Values("user", "admin"),
+		field.JSON("more_info", map[string]interface{}{}).Default(map[string]interface{}{}).Optional(),
 		field.Time("created_at").Immutable().Default(time.Now),
+		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
 	}
 }
 
